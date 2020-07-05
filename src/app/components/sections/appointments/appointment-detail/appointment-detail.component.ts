45bdf0dfcb8ec812fc3639b29c541d6b0ed21023
Rodrigo Ferretty <rodrigoferretty@gmail.com>
2020-07-05 09:19:54 -0300

    ngbDatePicker added for appointment. Restricting weekends and maxDate one month since today.

--- src/app/components/sections/appointments/appointment-detail/appointment-detail.component.ts
+++ src/app/components/sections/appointments/appointment-detail/appointment-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Appointment } from 'src/app/entities/appointment';
+import { NgbCalendar, NgbDateStruct, NgbDatepickerConfig, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-appointment-detail',
@@ -11,7 +12,23 @@ export class AppointmentDetailComponent implements OnInit {
   @Input('role') userRole: string;
   @Output() updateAppointment = new EventEmitter<Appointment>();
 
-  constructor() { }
+  model: NgbDateStruct;
+  date: { year: number, month: number };
+
+  constructor(private calendar: NgbCalendar, private ngbCalendarConfig: NgbDatepickerConfig) {
+    const current = new Date();
+    ngbCalendarConfig.minDate = {
+      year: current.getFullYear(), month:
+        current.getMonth() + 1, day: current.getDate()
+    };
+    ngbCalendarConfig.maxDate = {
+      year: current.getFullYear(), month:
+        current.getMonth() + 2, day: current.getDate()
+    };
+
+    ngbCalendarConfig.outsideDays = 'hidden';
+    ngbCalendarConfig.markDisabled = (date: NgbDate) => calendar.getWeekday(date) >= 6;
+  }
 
   ngOnInit(): void {
   }
